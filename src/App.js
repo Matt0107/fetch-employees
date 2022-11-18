ac6c1671afd117ebbcae1b240e2044d9e75e6ee4
Matt1789 <mathieu.durimel@gmail.com>
2022-11-18 22:34:53 +0100

    fetch with axios done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import DisplayEmployee from "./components/DisplayEmployee";
+import { useState } from "react";
+import axios from "axios";
+
+import React from "react";
 
 const sampleEmployee = {
   gender: "male",
@@ -22,7 +26,27 @@ const sampleEmployee = {
 };
 
 function App() {
-  return <DisplayEmployee employee={sampleEmployee} />;
+  const [employee, setEmployee] = useState(sampleEmployee);
+  const getEmployee = () => {
+    // Send the request
+    axios
+      .get("https://randomuser.me/api?nat=en")
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        setEmployee(data.results[0]);
+      });
+  };
+
+  return (
+    <div className="App">
+      <DisplayEmployee employee={employee} />
+      <button type="button" onClick={getEmployee}>
+        Get employee
+      </button>
+    </div>
+  );
 }
 
 export default App;
