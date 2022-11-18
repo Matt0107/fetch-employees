491f6fe5ce7e5ff1ecf952224b9d14f7acafb2ca
Matt1789 <mathieu.durimel@gmail.com>
2022-11-18 16:20:06 +0100

    sample employee created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+import DisplayEmployee from "./components/DisplayEmployee";
+
+const sampleEmployee = {
+  gender: "male",
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  location: {
+    street: {
+      number: 761,
+      name: "Tay Street",
+    },
+    city: "Timaru",
+    postcode: 76111,
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
 
 function App() {
-  return (
-    <div className="App">
-      <h1>simple test</h1>
-    </div>
-  );
+  return <DisplayEmployee employee={sampleEmployee} />;
 }
 
 export default App;
